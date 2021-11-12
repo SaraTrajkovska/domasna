522523a3f6a47ac47d4c7d1c1ccacf13490af7d5
SaraTrajkovska <93949800+SaraTrajkovska@users.noreply.github.com>
2021-11-13 00:57:14 +0100

    Delete promises.js

--- promises.js
+++
@@ -1,9 +0,0 @@
-const fs = require('fs/promises'); //promises modul
-const path = require('path');  
-const pathToFile = path.join(__dirname, 'http.txt'); // na koja pateka da odi
-const pathToWrittenFile = path.join(__dirname, 'newfile.txt'); //pateka kade da zapise i sodrzina
-
-module.exports = {
-    fileReader,
-    fileWriter,
-  };    //se eksportiraat za da se korsistat vo drugi fajlovi 
