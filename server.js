17108292f81f8feb46b6fde5842c8572634eed0d
SaraTrajkovska <93949800+SaraTrajkovska@users.noreply.github.com>
2021-11-13 21:41:51 +0100

    Add files via upload

---
+++ server.js
@@ -0,0 +1,7 @@
+const http = require("http")
+const concatenateStrings = require('./functions');
+const fileOperations = require('./promises');
+const Server = http.createServer;
+server.listen = http.createServer (8080,() => {console.log ('Zdravo')
+});
+// kreiran server na 8080 porta
